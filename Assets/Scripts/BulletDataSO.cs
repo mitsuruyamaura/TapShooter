bbfb480313d0e484d6e516cbc395d618112f80e1
mitsuru_yamaura <komayou326@gmail.com>
2021-01-10 14:17:18 +0900

    BulletTypeの名称修正と関連スクリプトのリファクタリング。ヘルパークラスの名称変更と関連スクリプトのリファクタリング。

--- Assets/Scripts/BulletDataSO.cs
+++ Assets/Scripts/BulletDataSO.cs
@@ -27,10 +27,10 @@ public class BulletDataSO : ScriptableObject {
         A,
         B,
         C,
-        D,
-        E,
-        F,
-        G,
+        Player_Normal,
+        Player_Blaze,
+        Player_3ways_Piercing,
+        Player_5ways_Normal,
         None
     }
 
