f94b5008cf042f2ea6eef596a0a4fcfa19a514b1
mitsuru_yamaura <komayou326@gmail.com>
2021-01-23 15:18:46 +0900

    バレット選択用ポップアップ用ゲームオブジェクト、選択用のボタンオブジェクトの作成。ChooseBulletPopUp.cs、ChooseBulletDetail.cs作成。 選択ボタン１タップで選択。２タップで設定。基本的な操作は実装済。次の手順で、選択リストの解除とバレット情報の表示反映を実装する。

---
+++ Assets/Scripts/ChooseBulletPopUp.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using System.Linq;
+
+public class ChooseBulletPopUp : MonoBehaviour
+{
+    [SerializeField]
+    private Text txtBulletDescription;
+
+    // 未選択のバレットのリスト
+    public List<ChooseBulletDetail> unselectedBulletDetailsList = new List<ChooseBulletDetail>();
+
+    // 登録中のバレットのリスト
+    public List<ChooseBulletDetail> choosingBulletDetailsList = new List<ChooseBulletDetail>();
+
+    [SerializeField]
+    private ChooseBulletDetail chooseBulletDetailPrefab;
+
+    [SerializeField]
+    private Transform bulletDetailTran;
+
+    [SerializeField]
+    private Transform selectBulletTran;
+
+    void Start()
+    {
+        CreateChooseBulletDetails();
+    }
+
+    /// <summary>
+    /// バレットのリスト作成
+    /// </summary>
+    public void CreateChooseBulletDetails() {
+        // プレイヤーのバレットのみ抽出
+        BulletDataSO.BulletData[] bulletDatas = DataBaseManager.instance.bulletDataSO.bulletDataList.Where((x) => x.liberalType == BulletDataSO.LiberalType.Player).ToArray();
+
+        Debug.Log(bulletDatas.Length);
+
+        // バレット生成
+        for (int i = 0; 0 < bulletDatas.Length; i++) {
+            if (i == 4) {
+                return;
+            }
+            ChooseBulletDetail chooseBulletDetail = Instantiate(chooseBulletDetailPrefab, selectBulletTran, false);
+            chooseBulletDetail.SetUpChooseBullet(this, ChooseBulletDetail.DetailStateType.Unselected, bulletDatas[i]);
+            unselectedBulletDetailsList.Add(chooseBulletDetail);
+        }
+    }
+
+    /// <summary>
+    /// バレットの性能の表示更新
+    /// </summary>
+    /// <param name="bulletData"></param>
+    public void UpdateDisplayBulletPerformance(BulletDataSO.BulletData bulletData) {
+        txtBulletDescription.text = bulletData.discription;
+
+    }
+
+    /// <summary>
+    /// 利用可能なバレットのリストに追加
+    /// </summary>
+    public void AddChooseBulletDetail(ChooseBulletDetail chooseBulletDetail) {
+        // 生成
+        ChooseBulletDetail chooseBulletDetailCopy = Instantiate(chooseBulletDetail, bulletDetailTran, false);
+
+        chooseBulletDetail.SetUpChooseBullet(this, ChooseBulletDetail.DetailStateType.Choosing, chooseBulletDetailCopy.bulletData);
+        choosingBulletDetailsList.Add(chooseBulletDetailCopy);
+    }
+
+    /// <summary>
+    /// 選択したバレットをリストから削除
+    /// </summary>
+    /// <param name="chooseBulletDetail"></param>
+    public void DeleteChoosingBulletList(ChooseBulletDetail chooseBulletDetail) {
+        foreach (ChooseBulletDetail chooseBullet in unselectedBulletDetailsList) {
+            if (chooseBullet.bulletData.bulletType == chooseBulletDetail.bulletData.bulletType) {
+
+                // タップできる状態に戻す
+                chooseBullet.ActivateBtn(true);
+
+                // ステートを初期化
+                chooseBullet.SetState(ChooseBulletDetail.DetailStateType.Unselected);
+            }
+        }
+        Destroy(chooseBulletDetail);
+
+        choosingBulletDetailsList.Remove(chooseBulletDetail);
+    }
+
+    /// <summary>
+    /// 選択しているバレットリストをすべて削除
+    /// </summary>
+    public void ResetChoosingBulletList() {
+        if (choosingBulletDetailsList.Count > 0) {
+            return;
+        }
+
+        for (int i = 0; 0 < choosingBulletDetailsList.Count; i++) {
+            Destroy(choosingBulletDetailsList[i]);
+        }
+
+        choosingBulletDetailsList.Clear();
+    }
+}
